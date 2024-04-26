560cf720f7d9419573a6d9af55014e714990fb57
Ana Vargas <avargas2020@inf.udec.cl>
2024-04-26 10:57:28 -0400

    Codigo para entrenar al agente

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import random
 import time
 import numpy as np
 
+from gym import Env
+from gym.spaces import Discrete, Box
+import cv2
+
 game = DoomGame()
 game.load_config(".venv/Lib/site-packages/vizdoom/scenarios/basic.cfg")
 game.init()
 
-# Acciones que puede realizar el agente
+# # Acciones que puede realizar el agente
 actions = np.identity(3, dtype=np.uint8)
 random.choice(actions)
 
@@ -28,4 +32,62 @@ for episode in range(episodes):
         time.sleep(0.02)
     print("Result:", game.get_total_reward())
     time.sleep(2)
-game.close()
+
+#Creación del ambiente de entrenamiento
+# class VizDoomGym(Env):
+#     def __init__(self, render=False):
+#         super().__init__()
+#         self.game = DoomGame()
+#         self.game.load_config(".venv/Lib/site-packages/vizdoom/scenarios/basic.cfg")
+
+#         if render == False:
+#             self.game.set_window_visible(False)
+#         else:
+#             self.game.set_window_visible(True)
+
+#         #Inicializa el juego
+#         self.game.init()
+
+#         self.observation_space = Box(low=0, high=255, shape=(100, 160, 1), dtype=np.uint8)
+#         self.action_space = Discrete(3)
+
+#     def step(self, action):
+#         #Especifica las acciones que el agente puede realizar
+#         actions = np.identity(3, dtype=np.uint8)
+#         reward = self.game.make_action(actions[action], 4)
+
+#         #Obtiene las demás variables 
+#         if self.game.get_state():
+#             state = self.game.get_state().screen_buffer
+#             state = self.grayscale(state)
+#             info = self.game.get_state().game_variables
+#         else:
+#             state = np.zeros(self.observation_space.shape)
+#             info = 0
+
+#         done = self.game.is_episode_finished()
+
+#         return state, reward, done, info
+
+#     def render():
+#         pass
+    
+#     def reset(self):
+#         self.game.new_episode()     
+#         state = self.game.get_state().screen_buffer
+#         return self.grayscale(state)
+
+#     def grayscale(self, observation):
+#         gray = cv2.cvtColor(np.moveaxis(observation, 0, -1), cv2.COLOR_BGR2GRAY)
+#         resize = cv2.resize(gray, (160, 100), interpolation=cv2.INTER_CUBIC)
+#         state = np.reshape(resize, (100, 160, 1))
+#         return state
+
+#     def close(self):
+#         self.game.close()
+
+# env = VizDoomGym(render=True)
+# state = env.reset()
+# np.moveaxis(state, 0, -1).shape
+# env.step(2)
+# env.close(
